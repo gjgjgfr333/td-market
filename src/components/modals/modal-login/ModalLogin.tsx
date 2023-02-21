b0e78360394041eadb9498071668278febe9d627
Ilya Dvorchuk <iladvorchuk@gmail.com>
2023-02-21 22:01:18 +0200

    feat: реализация модального окна юзера

--- src/components/modals/modal-login/ModalLogin.tsx
+++ src/components/modals/modal-login/ModalLogin.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './modal-login.scss'
 import Cover from "../../cover/Cover";
 import {useAppDispatch} from "../../../hooks/redux";
@@ -7,6 +7,16 @@ import {userSlice} from "../../../store/reducers/user/UserSlice";
 const ModalLogin = () => {
     const {changeIsUserModal} = userSlice.actions
     const dispatch = useAppDispatch()
+
+    const [isAgree, setIsAgree] = useState(false)
+
+    const onRegistry = () => {
+        if (!isAgree) return
+    }
+
+    const onAgreeRules = () => {
+        setIsAgree(!isAgree)
+    }
 
     const closeUserModal = () => {
         dispatch(changeIsUserModal(false))
@@ -18,7 +28,14 @@ const ModalLogin = () => {
                 <h3 className={'modalLogin__title'}>Войти или создать аккаунт</h3>
                 <input type="email" className={'modalLogin__email'} placeholder={'E-mail...'}/>
                 <button className={'button modalLogin__enter'}>ВОЙТИ</button>
-                <button className={'button modalLogin__registry'}>ЗАРЕГИСТРИРОВАТЬСЯ</button>
+                <button className={'button modalLogin__registry'} onClick={onRegistry}>ЗАРЕГИСТРИРОВАТЬСЯ</button>
+                <div className={'modalLogin__row'}>
+                    <input type={'checkbox'} className={'modalLogin__checkBox'} onChange={onAgreeRules}/>
+                    <p className={'modalLogin__conditions'}>
+                        Согласен с условиями, <a href={'/'} className={'modalLogin__link'}>правилами возврата</a> и
+                        <a href="/" className={'modalLogin__link'}> правилами пользования торговой площадкой</a>.
+                    </p>
+                </div>
             </div>
             <Cover callback={closeUserModal}/>
         </>
