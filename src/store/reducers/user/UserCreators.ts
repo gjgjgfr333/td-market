7c6ada881dad8c8aa1d72e8eadaa1f3711f7ff4b
Ilya Dvorchuk <iladvorchuk@gmail.com>
2023-03-06 23:15:28 +0200

    feat: бизнес логика запросов

---
+++ src/store/reducers/user/UserCreators.ts
@@ -0,0 +1,63 @@
+import {AppDispatch} from "../../store";
+
+import {userSlice} from "./UserSlice";
+import {AuthService} from "../../../services/AuthService";
+import {IUser} from "../../../models/IUser";
+import axios from "axios";
+import {IAuthResponse} from "../../../models/response/IAuthResponse";
+import {API_URL} from "../../../http";
+
+export const loginUser = (email: string, password: string) => async (dispatch: AppDispatch) => {
+    try {
+        dispatch(userSlice.actions.loginFetching())
+        const response = await AuthService.login(email, password)
+        localStorage.setItem('token', response.data.accessToken)
+        dispatch(userSlice.actions.setAuth(true))
+        dispatch(userSlice.actions.setUser(response.data))
+        dispatch(userSlice.actions.loginSuccess())
+    } catch (e: any) {
+        console.log('e', e)
+        dispatch(userSlice.actions.loginSuccess(e.message))
+    }
+}
+
+export const registrationUser = (email: string, password: string) => async (dispatch: AppDispatch) => {
+    try {
+        dispatch(userSlice.actions.loginFetching())
+        const response = await AuthService.registration(email, password)
+        localStorage.setItem('token', response.data.accessToken)
+        dispatch(userSlice.actions.setAuth(true))
+        dispatch(userSlice.actions.setUser(response.data))
+        dispatch(userSlice.actions.loginSuccess())
+    } catch (e: any) {
+        console.log('e', e)
+        dispatch(userSlice.actions.loginSuccess(e.message))
+    }
+}
+
+export const logout = () => async (dispatch: AppDispatch) => {
+    try {
+        dispatch(userSlice.actions.loginFetching())
+        await AuthService.logout()
+        localStorage.removeItem('token')
+        dispatch(userSlice.actions.setAuth(false))
+        dispatch(userSlice.actions.setUser({} as IUser))
+        dispatch(userSlice.actions.loginSuccess())
+    } catch (e: any) {
+        console.log('e', e)
+        dispatch(userSlice.actions.loginSuccess(e.message))
+    }
+}
+
+export const checkAuth = () => async (dispatch: AppDispatch) => {
+    try {
+        const response = await axios.get<IAuthResponse>(`${API_URL}/refresh`, {withCredentials: true})
+        localStorage.removeItem('token')
+        localStorage.setItem('token', response.data.accessToken)
+        dispatch(userSlice.actions.setAuth(true))
+        dispatch(userSlice.actions.setUser(response.data))
+    } catch (e: any) {
+        console.log('e', e)
+        dispatch(userSlice.actions.loginSuccess(e.message))
+    }
+}
--- src/store/reducers/user/UserSlice.ts
+++ src/store/reducers/user/UserSlice.ts
@@ -1,16 +1,41 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
 const initialState = {
+    user: {},
     isUserModal: false,
+    isAuth: false,
+    isLoading: false,
+    error: '',
 }
 
 export const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
-        changeIsUserModal: (state, action) => {
+        loginFetching(state) {
+            state.isLoading = true
+        },
+
+        loginSuccess(state) {
+            state.isLoading = true
+        },
+
+        loginFetchingError(state, action: PayloadAction<string>) {
+            state.isLoading = false
+            state.error = action.payload
+        },
+
+        changeIsUserModal: (state, action: PayloadAction<boolean>) => {
             state.isUserModal = action.payload
-        }
+        },
+
+        setAuth: (state, action: PayloadAction<boolean>) => {
+            state.isAuth = action.payload
+        },
+
+        setUser: (state, action) => {
+            state.user = action.payload
+        },
     }
 })
 
