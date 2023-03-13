4e2530b222f7f3368581d0593ee44a4d9990b862
Ilya Dvorchuk <iladvorchuk@gmail.com>
2023-03-13 22:52:37 +0200

    feat: работа над тайтлом

--- src/components/title-cards/TitleCards.tsx
+++ src/components/title-cards/TitleCards.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './title-cards.scss'
 
 const TitleCards = ({text}: {text: string}) => {
     return (
-        <h2>
-            {text}
-            <img src='/images/svg/arrow-right.svg' alt=""/>
+        <h2 className={'title'}>
+            <span>{text}</span>
+            <img src='/images/svg/arrow-right-fat.svg' alt=""/>
         </h2>
     );
 };
