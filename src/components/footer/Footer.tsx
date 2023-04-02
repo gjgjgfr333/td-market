3dd000d27681fe62338b036a7e6b269dc0f5165c
Ilya Dvorchuk <iladvorchuk@gmail.com>
2023-04-02 15:54:11 +0300

    feat: работа над стилями футера

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,12 +7,41 @@ const Footer = () => {
     return (
         <footer className={'footer'}>
             <Container>
-                <div className={'footer__column'}>
-                    <h3>Покупателю</h3>
-                    <Link to={'/'}>Как сделать покупку</Link>
-                    <Link to={'/'}>Возврат товара</Link>
-                    <Link to={'/'}>Возврат денежных товаров</Link>
-                    <Link to={'/'}>Поддержка</Link>
+                <div className={'footer__wrapper'}>
+                    <div className={'footer__column'}>
+                        <h3 className={'footer__title'}>Покупателям</h3>
+                        <Link to={'/'} className={'footer__link'}>Как сделать покупку</Link>
+                        <Link to={'/'} className={'footer__link'}>Возврат товара</Link>
+                        <Link to={'/'} className={'footer__link'}>Возврат денежных товаров</Link>
+                        <Link to={'/'} className={'footer__link'}>Поддержка</Link>
+                    </div>
+                    <div className={'footer__column'}>
+                        <h3 className={'footer__title'}>Продавцам</h3>
+                        <Link to={'/'} className={'footer__link'}>Продавайте на td-market</Link>
+                        <Link to={'/'} className={'footer__link'}>Личный кабинет продавца</Link>
+                        <Link to={'/'} className={'footer__link'}>Документация</Link>
+                    </div>
+                    <div className={'footer__column'}>
+                        <h3 className={'footer__title'}>Компания</h3>
+                        <Link to={'/'} className={'footer__link'}>О нас</Link>
+                        <Link to={'/'} className={'footer__link'}>Вопрос - ответ</Link>
+                        <Link to={'/'} className={'footer__link'}>Договор публичной оферты</Link>
+                    </div>
+                    <div className={'footer__column'}>
+                        <h3 className={'footer__title'}>Мы в соцсетях</h3>
+                        <div className={'footer__icons'}>
+                            <a href={'/'} target={"_blank"}>
+                                <img src="/images/svg/telegram.svg" alt="Telegram"/>
+                            </a>
+                            <a href={'/'} target={"_blank"}>
+                                <img src="/images/svg/instagram.svg" alt="Telegram"/>
+                            </a>
+                            <a href={'/'} target={"_blank"}>
+                                <img src="/images/svg/telegram.svg" alt="Telegram"/>
+                            </a>
+                        </div>
+                        <p className={'footer__text'}>Информация, предоставленная на сайте, не является публичной офертой. Все права защищены.</p>
+                    </div>
                 </div>
             </Container>
         </footer>
