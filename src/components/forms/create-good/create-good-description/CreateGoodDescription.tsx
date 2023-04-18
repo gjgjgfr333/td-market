579296332df6bdb1bee5442df85d471186775004
Ilya Dvorchuk <iladvorchuk@gmail.com>
2023-04-18 23:22:13 +0300

    feat: подключаю маркдаун

--- src/components/forms/create-good/create-good-description/CreateGoodDescription.tsx
+++ src/components/forms/create-good/create-good-description/CreateGoodDescription.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import './create-good-description.scss'
+import '../../../../styles/elements/inputs.scss'
+import {useForm} from "react-hook-form";
+import ReactMarkdown from "react-markdown";
 
 const CreateGoodDescription = () => {
-    return (
-        <div>
+    const {register, handleSubmit} = useForm()
 
-        </div>
+    const onSubmit = () => {
+
+    }
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)} className={'description'}>
+            <div className={'description__block'}>
+                <label className={'label'} htmlFor="name">Название</label>
+                <input
+                    id={'name'}
+                    placeholder={'Введите название товара'}
+                    className={'modalInput description__input'}
+                    {...register('Название')}
+                />
+            </div>
+            <div>
+                <label className={'label'} htmlFor="">Описание</label>
+                <ReactMarkdown>
+                    # Hello, *world*!
+                </ReactMarkdown>
+            </div>
+        </form>
     );
 };
 
