941169294577171d48fe63f45609a5e1f88b60a1
Ilya Dvorchuk <iladvorchuk@gmail.com>
2023-06-11 19:10:24 +0300

    fix: проверка метода

--- src/store/reducers/categories/CategoriesCreators.ts
+++ src/store/reducers/categories/CategoriesCreators.ts
@@ -2,11 +2,15 @@ import {AppDispatch} from "../../store";
 import axios from "axios";
 import {ICategory} from "../../../models/ICategories";
 import {categoriesSlice} from "./CategoriesSlice";
+import * as https from "https";
 
 export const fetchCategories = () => async (dispatch: AppDispatch) => {
     try {
         dispatch(categoriesSlice.actions.categoriesFetching())
-        const response = await axios.get<ICategory[]>('http://45.84.0.197:5000/categories')
+        const response = await axios.get<ICategory[]>('https://45.84.0.197:5000/categories', {
+            // Необязательно, но может понадобиться, чтобы пропустить проверку сертификата
+            httpsAgent: new https.Agent({ rejectUnauthorized: false })
+        });
         dispatch(categoriesSlice.actions.categoriesFetchingSuccess(response.data))
     } catch (e: any) {
         console.log('e', e)
