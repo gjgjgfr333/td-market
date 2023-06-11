4beec91790ca3cb2e9d886e60c8b2d7fa14ee2bd
Ilya Dvorchuk <iladvorchuk@gmail.com>
2023-06-11 18:53:21 +0300

    fix: проверка метода

--- src/store/reducers/categories/CategoriesCreators.ts
+++ src/store/reducers/categories/CategoriesCreators.ts
@@ -6,7 +6,7 @@ import {categoriesSlice} from "./CategoriesSlice";
 export const fetchCategories = () => async (dispatch: AppDispatch) => {
     try {
         dispatch(categoriesSlice.actions.categoriesFetching())
-        const response = await axios.get<ICategory[]>('https://45.84.0.197:5000/categories')
+        const response = await axios.get<ICategory[]>('http://45.84.0.197:5000/categories')
         dispatch(categoriesSlice.actions.categoriesFetchingSuccess(response.data))
     } catch (e: any) {
         console.log('e', e)
