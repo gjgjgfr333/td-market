6f6b1688371c339b18fb3330bbd3ddcc34b27141
Ilya Dvorchuk <iladvorchuk@gmail.com>
2023-06-19 19:37:59 +0300

    fix: не надо перегружать сайт для фаворитов

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -2,17 +2,16 @@ import React, {useEffect} from 'react';
 import Header from "../components/headers/header/Header";
 import Container from "../components/container/Container";
 import BoxFavorites from "../components/boxes/box-favorites/BoxFavorites";
-import {useAppSelector} from "../hooks/redux";
 import {useNavigate} from "react-router-dom";
 import Footer from "../components/footers/footer-user/Footer";
+import {getAccessTokenUser} from "../utils/tokens";
 
 const Favorites = () => {
     const navigation = useNavigate()
-    const accessToken = useAppSelector((state) => state.userReducer.accessToken);
 
     useEffect(() => {
-        if (!accessToken) navigation('/')
-    }, [accessToken, navigation])
+        if (!getAccessTokenUser()) navigation('/')
+    }, [ navigation])
 
     return (
         <div>
